d53ee14cd6163c8d9b80680dbeeacc828a28d4f9
Mysta3 <myshawnestallings@gmail.com>
2020-03-22 21:58:59 -0700

    installed webpack, babel, & created initial react component

--- djsr/djsr/settings.py
+++ djsr/djsr/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authentication',
-    'rest_framework'
+    'rest_framework',
+    'frontend'
 ]
 
 REST_FRAMEWORK = {
