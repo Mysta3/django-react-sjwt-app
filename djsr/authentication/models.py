cb5ecee5c2b099b82921629f3d3e9c3164f04bec
Mysta3 <myshawnestallings@gmail.com>
2020-03-22 20:36:25 -0700

    mirgated initial modles

--- djsr/authentication/models.py
+++ djsr/authentication/models.py
@@ -7,5 +7,5 @@ from django.db import models
 # Customer extends from AbstractUser which gives us access to the standard Django User model attributes and functionalities such as username, password, etc.
 
 #don't need to add the other fields because they already exist in the user model.
-class Customer(AbstractUser):
+class CustomUser(AbstractUser):
     fav_color = models.CharField(blank=True, max_length=120)
